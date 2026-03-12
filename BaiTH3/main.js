66f2b748c07c0aca02a2271262fbe43cdfa29fee
Ha Xuan Hai <Haxuanhai10a1@gmail.com>
2026-03-12 20:43:21 +0700

    add baiTH3

---
+++ BaiTH3/main.js
@@ -0,0 +1,6 @@
+console.log("Hello from JavaScript!");let name = "Hai";
+let yearOfBirth = 2006;
+let currentYear = 2026;
+let age = currentYear - yearOfBirth;
+
+console.log("Xin chào, mình là " + name + ", năm nay mình " + age + " tuổi.");
